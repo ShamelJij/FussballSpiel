770e7728647fe4416980f9dce57e6d9f37c0a46c
shameljij <shamel.jij@gmail.com>
2021-06-27 14:13:20 +0200

    Text Adventure

---
+++ src/Spiel/Example.java
@@ -0,0 +1,5 @@
+package Spiel;
+
+public class Example {
+
+}
---
+++ src/Spiel/Spiel.java
@@ -0,0 +1,22 @@
+
+package Spiel;
+import Spiel.Example;
+
+public class Spiel {
+    public void method() {
+        String something = "whatever";
+        System.out.println(something);
+    }
+
+
+
+    public static void main(String[] args) {
+        Spiel spiel = new Spiel();
+        spiel.method();
+
+    }
+
+
+
+}
+
